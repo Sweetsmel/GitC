a4043e0f198bcdac69da54bce13b7730f1d501cf
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-23 09:08:10 -0300

    SelectQuantidadeMarcas

---
+++ QuerysInnerJoin/SQLQuery1.sql
@@ -0,0 +1,21 @@
+﻿select * 
+	from Marcas Mr 
+	inner join Carros Cr on Mr.Id = Cr.Marcas
+order by Marcas desc
+
+	--select * 
+	--from Turmas tr 
+	--inner join TurmaAlunos ta on tr.Id = ta.Turma
+
+select n.Nome as 'Marcas',
+		m.Marcas
+	from Marcas n 
+	inner join Marcas mr on n.Id = mr.Nome
+	inner join Usuarios m on mr.Usuario = a.Id
+
+
+select tr.Nome as 'Turma',
+	a.Aluno
+from Turmas tr 
+inner join TurmaAlunos ta on tr.Id = ta.Turma
+inner join Alunos a on ta.Aluno = a.Id
