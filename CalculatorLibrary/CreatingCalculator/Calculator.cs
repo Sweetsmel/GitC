b492d142fd6fd8cb1c051a587355f16dfa047a80
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 17:19:57 -0300

    Calculando Operações básicas.

---
+++ CalculatorLibrary/CreatingCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary.CreatingCalculator
+{
+    public class Calculator
+    {
+        public int CalculatorAdicao(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+        public int CalculatorSubtracao(int valor1, int valor2)
+        {
+            return valor1 - valor2;
+        }
+        public int CalculatorMultiplicacao(int valor1, int valor2)
+        {
+            return valor1 * valor2;
+        }
+        public int CalculatorDivisao(int valor1, int valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
---
+++ ProjectUsingCalculatorLibrary/Program.cs
@@ -0,0 +1,25 @@
+﻿using CalculatorLibrary.CreatingCalculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectUsingCalculatorLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator bbCalc = new Calculator();
+
+            Console.WriteLine("Calculando operações básicas:");
+            Console.WriteLine($"\r\nAdição:\r\n5 + 5 = {bbCalc.CalculatorAdicao(5, 5)}");
+            Console.WriteLine($"\r\nSubtração:\r\n5 - 5 = {bbCalc.CalculatorSubtracao(5, 5)}");
+            Console.WriteLine($"\r\nMultiplicação:\r\n5 * 5 = {bbCalc.CalculatorMultiplicacao(5, 5)}");
+            Console.WriteLine($"\r\nDivisão:\r\n5 / 5 = {bbCalc.CalculatorDivisao(5, 5)}");
+
+            Console.ReadKey();
+        }
+    }
+}
